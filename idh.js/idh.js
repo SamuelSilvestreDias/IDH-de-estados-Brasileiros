86fb7418742c03d03dffe0c9b1c1c7384c127b59
SamuelSilvestreDias <106530810+SamuelSilvestreDias@users.noreply.github.com>
2022-09-05 07:43:07 -0300

    Add files via upload
    Código em java Script de um medidos de IDH de estados BR.

---
+++ idh.js/idh.js
@@ -0,0 +1,19 @@
+console.log('Estados Brasileiros: ');
+let Capital;
+let EstadosBrasileiros;
+let SãoPaulo;
+function EstadosBrasileiros (SãoPaulo){
+    
+function SãoPaulo () { 
+   var Tamanho = "248209²";
+   var NumdeHabitantes = "44.04 Milhões";
+   var IDH = "783 Milhões";
+    function Capital () {
+     var Nome = "São Paulo";  
+     var TamanhoEmKmQuadrados = "1521²";
+     var  NumDeHabitantes = "12.33Milhões";
+     var  IDHEM2020 = "12.33 Milhões";
+      }    
+    
+    }
+}
